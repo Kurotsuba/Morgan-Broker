f9de050e094a48f9be6878f80751a26ff58eddc0
Kurotsuba <kanneco98@gmail.com>
2019-05-20 13:04:21 +0800

    Switch to Spring Boot

--- src/OrderServiceImpl.java
+++
@@ -1,2 +0,0 @@
-public class OrderServiceImpl implements OrderService {
-}
---
+++ src/main/java/group/eis/morganborker/MorganBrokerApplication.java
@@ -0,0 +1,13 @@
+package group.eis.morganborker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorganBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorganBrokerApplication.class, args);
+    }
+
+}
